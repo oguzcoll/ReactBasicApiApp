5a295e5c9b0aa85f9ada09bb2f45a5e8c03112a2
Oguz Colak <oguzcolak2000@gmail.com>
2023-02-14 15:43:42 +0300

    first commit

---
+++ src/SearchHeader.js
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+function SearchHeader({search}) {
+ const [valueInput, setValueInput] = useState('')
+ const handleFormSubmit = (event) =>{
+    event.preventDefault();
+    search(valueInput)
+ }
+const handleChange = (event)=>{
+    setValueInput(event.target.value);
+}
+
+  return (
+    <div className = "searchDiv">
+      <form onSubmit = {handleFormSubmit}>
+        <label>What Are  You Looking For?</label>
+        <input value={valueInput} onChange={handleChange}/>
+      </form>
+    </div>
+  );
+}
+
+export default SearchHeader;
